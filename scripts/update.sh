c22268de3cc4f7302e03f5f6e8616dfaf543260a
Roelof Roos <github@roelofr.dev>
2022-08-23 16:54:47 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+test -d ~/bin || mkdir ~/bin
+
+set -e
+
+# Download updater
+echo "Downloading updater"
 curl \
     --fail \
     --location \
     --output ~/bin/update-plazacam.sh \
     https://github.com/gumbo-millennium/plazacam/raw/main/scripts/update.sh
 
+# Download capture
+echo "Downloading capture script"
 curl \
     --fail \
     --location \
     --output ~/bin/capture-plazacam.sh \
     https://github.com/gumbo-millennium/plazacam/raw/main/scripts/capture.sh
 
+# Create device ID
+if [ ! -f $HOME/device-id.txt ]; then
+    echo "Creating device ID"
+    cat /proc/sys/kernel/random/uuid > $HOME/device-id.txt
+fi
+
+# Fix permissions
 chmod u+x ~/bin/capture-plazacam.sh ~/bin/update-plazacam.sh
